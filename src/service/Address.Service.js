d50e3c11b2c44c180d010455277187d4953cadd3
Phansit <phansit.son@gmail.com>
2023-03-08 15:08:47 +0700

    Update Address.Service.js

--- src/service/Address.Service.js
+++ src/service/Address.Service.js
@@ -3,7 +3,9 @@ import address_thai from "../data/address_thai.json";
 export function  getAddressThai (search) {
     let res = address_thai;
     if(search){
-        res = res.filter((a) => a.SubdistrictsNameTh.includes(search) || a.DistrictsNameTh.includes(search) || a.ProvincesNameTh.includes(search) || a.PostCode.includes(search));
+        
+        res = res.filter((a) =>  a.SubdistrictsNameTh.includes(search) || a.DistrictsNameTh.includes(search) || a.ProvincesNameTh.includes(search) || a.PostCode.includes(search));
     }
-    return res;
+    console.log(res.length)
+    return res.slice(0,99);
 }
