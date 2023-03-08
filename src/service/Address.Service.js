cf10b17ed5aa2d7b56898199b1dd991ac9aea0f8
jakkritdokkem <boom0928754237@gmail.com>
2023-03-08 20:40:43 +0700

    change wording

--- src/service/Address.Service.js
+++ src/service/Address.Service.js
@@ -1,9 +1,9 @@
-import address_thai from "../data/address.json";
+import Address from '../data/address.json';
 
-export function  getAddressThai (search) {
-    let res = address_thai;
-    if(search){
-        res = res.filter((a) =>  a.SubdistrictsNameTh.includes(search) || a.DistrictsNameTh.includes(search) || a.ProvincesNameTh.includes(search) || a.PostCode.includes(search));
-    }
-    return res.slice(0,99);
+export function getAddressThai(search) {
+  let res = Address;
+  if (search) {
+    res = res.filter((a) => a.SubdistrictsNameTh.includes(search) || a.DistrictsNameTh.includes(search) || a.ProvincesNameTh.includes(search) || a.PostCode.includes(search));
+  }
+  return res.slice(0, 99);
 }
